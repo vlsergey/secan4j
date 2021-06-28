54a874372d614fb198c181c12448dfafc5dea6f6
Sergey Vladimirov <vlsergey@gmail.com>
2021-06-29 00:04:11 +0300

    WIP: Complete test case for SimpleColoredMethods.append(…)

--- secan4j-core/src/test/java/io/github/vlsergey/secan4j/core/session/PaintingSessionTest.java
+++ secan4j-core/src/test/java/io/github/vlsergey/secan4j/core/session/PaintingSessionTest.java
@@ -1,32 +1,40 @@
 package io.github.vlsergey.secan4j.core.session;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.fail;
 
-import java.util.Arrays;
+import java.util.function.BiConsumer;
 
 import org.junit.jupiter.api.Test;
 
+import io.github.vlsergey.secan4j.core.colored.ColorType;
 import io.github.vlsergey.secan4j.core.colored.ColoredObject;
 import io.github.vlsergey.secan4j.core.colored.SimpleColoredMethods;
+import io.github.vlsergey.secan4j.core.colored.TraceItem;
 import javassist.ClassPool;
 import javassist.CtClass;
 import javassist.CtMethod;
+import lombok.NonNull;
 
 class PaintingSessionTest {
 
 	private final ClassPool classPool = ClassPool.getDefault();
+
+	private static final @NonNull BiConsumer<TraceItem, TraceItem> noIntersectionExpected = (source, sink) -> {
+		fail("We din't expect intersection to be found so soon");
+	};
 
 	@Test
 	void testAppend() throws Exception {
 		final CtClass ctClass = classPool.get(SimpleColoredMethods.class.getName());
 		final CtMethod ctMethod = ctClass.getDeclaredMethod("append");
 
-		PaintingSession paintingSession = new PaintingSession(classPool, (source, sink) -> {
-			fail("We din't expect intersection to be found so soon");
-		});
+		PaintingSession paintingSession = new PaintingSession(classPool, noIntersectionExpected);
 
 		final ColoredObject[][] result = paintingSession.analyze(ctMethod);
-		System.out.println(Arrays.toString(result));
+		assertNotNull(result);
+		assertEquals(ColorType.SourceData, result[0][2].getColor().getType());
 	}
 
 	@Test
@@ -34,12 +42,11 @@ class PaintingSessionTest {
 		final CtClass ctClass = classPool.get(SimpleColoredMethods.class.getName());
 		final CtMethod ctMethod = ctClass.getDeclaredMethod("concatenation");
 
-		PaintingSession paintingSession = new PaintingSession(classPool, (source, sink) -> {
-			fail("We din't expect intersection to be found so soon");
-		});
+		PaintingSession paintingSession = new PaintingSession(classPool, noIntersectionExpected);
 
 		final ColoredObject[][] result = paintingSession.analyze(ctMethod);
-		System.out.println(result);
+		assertNotNull(result);
+//		assertEquals(ColorType.SourceData, result[1][0].getColor().getType());
 	}
 
 }
